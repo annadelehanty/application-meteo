fc2d624d7610e1a72d2ecf7e0ad85e99dbb5b52b
annadelehanty <agmdelehanty@gmail.com>
2024-03-08 18:13:23 +0000

    made the date dynamic

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
+function formatDate(date) {
+  let days = [
+    `sunday`,
+    `monday`,
+    `tuesday`,
+    `wednesday`,
+    `thursday`,
+    `friday`,
+    `saturday`,
+  ];
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function refreshWeather(data) {
   let tempNow = document.querySelector("#temp-now");
   tempNow.innerHTML = Math.round(data.temperature.current);
@@ -9,8 +30,10 @@ function refreshWeather(data) {
   windSpeed.innerHTML = data.wind.speed;
   const image = document.getElementById("current-icon");
   image.setAttribute("src", data.condition.icon_url);
+  let date = document.querySelector("#date");
+  let dateData = new Date(data.time * 1000);
+  date.innerHTML = formatDate(dateData);
 }
-function adjustTextSize() {}
 function searchCity(cityInput) {
   let apiKey = "4288f539432426do920341baabbb0tad";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
@@ -31,4 +54,5 @@ function handleSearch(event) {
 }
 let form = document.querySelector("form");
 form.addEventListener("submit", handleSearch);
-searchCity("LONDON");
+searchCity("london");
+setInterval(refreshWeather, 1000);
