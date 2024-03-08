431b4b1cc305ca5823480b5536975da729504ba4
annadelehanty <agmdelehanty@gmail.com>
2024-03-08 16:27:39 +0000

    added js to change the title and current temp

--- src/script.js
+++ src/script.js
@@ -1,9 +1,21 @@
+function refreshWeather(data) {
+  let tempNow = document.querySelector("#temp-now");
+  tempNow.innerHTML = Math.round(data.temperature.current);
+}
+function searchCity(cityInput) {
+  let apiKey = "4288f539432426do920341baabbb0tad";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(function (response) {
+    refreshWeather(response.data);
+  });
+}
 function handleSearch(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
+  let cityInput = document.querySelector("#city-input").value;
   let location = document.querySelector("#location");
-  cityInput = cityInput.value.toUpperCase();
-  location.innerHTML = cityInput;
+  location.innerHTML = cityInput.toUpperCase();
+  searchCity(cityInput);
 }
 let form = document.querySelector("form");
 form.addEventListener("submit", handleSearch);
+searchCity("LONDON");
